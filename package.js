6c3f12f81252c8dab98ddc040b5c7a43307f7e6d
Marcelo Reyna <Lepozepo@gmail.com>
2014-10-02 01:34:05 -0500

    Bring it up to speed

--- package.js
+++ package.js
@@ -1,14 +1,19 @@
 Package.describe({
-  summary: "Publish associated collections at once."
+	name:"lepozepo:publish-with-relations",
+	summary: "Publish associated collections at once.",
+	version:"1.0.0",
+	git:"https://github.com/Lepozepo/meteor-publish-with-relations"
 });
 
 Package.on_use(function(api) {
-  api.use('coffeescript', 'server');
-  api.add_files('publish_with_relations.coffee', 'server');
+	api.versionsFrom('METEOR@0.9.2');
+
+	api.use('coffeescript', 'server');
+	api.add_files('publish_with_relations.coffee', 'server');
 });
 
 Package.on_test(function(api) {
-  api.use('publish-with-relations');
-  
-  api.add_files('publish_with_relations_test.coffee', 'server');
+	api.use('publish-with-relations');
+
+	api.add_files('publish_with_relations_test.coffee', 'server');
 });
